76a59e044309dedfdb320b23cf4caba8c6ee59a4
Tin Nguyen <you@example.com>
2021-02-26 06:59:38 +0700

    first commit

---
+++ Tesseract ORC/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace Tesseract_ORC
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.picloading = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picloading)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(63, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Chọn ảnh";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(407, 65);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Chuyển đổi";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(51, 147);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(315, 208);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(388, 147);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(359, 219);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // picloading
+            // 
+            this.picloading.Location = new System.Drawing.Point(628, 53);
+            this.picloading.Name = "picloading";
+            this.picloading.Size = new System.Drawing.Size(100, 50);
+            this.picloading.TabIndex = 4;
+            this.picloading.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(186, 65);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(147, 23);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "Chọn file PDF";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 389);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.picloading);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.picloading)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox picloading;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Tesseract ORC/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tesseract;
+using ImageMagick;
+
+namespace Tesseract_ORC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private string OCR(Pix b)
+        {
+            string res = "";
+            using (var engine = new TesseractEngine(@"tessdata", "vie", EngineMode.Default))
+            {
+                
+                using (var page = engine.Process(b, PageSegMode.AutoOnly))
+                    res = page.GetText();
+            }
+            return res;
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                
+                using (var img = new MagickImage(openFileDialog1.FileName))
+                {
+
+                    img.ColorFuzz = new Percentage(15);
+                    // -transparent white
+                    img.Transparent(MagickColors.White);
+
+
+                    //img.Threshold((Percentage)60); // 60 is OK 
+                    //img.Depth = 1;
+
+                    //-bordercolor black
+                    //img.BorderColor = MagickColors.Black;
+
+                    //img.Border(1);
+
+                    //img.FloodFill(MagickColors.White, 0, 0);
+                    // -alpha off
+
+
+
+
+                    img.Alpha(AlphaOption.Off);
+                    // -shave 1x1
+                    img.Shave(1, 1);
+
+
+                    img.Write("img/image.png");
+                    
+                }
+
+                pictureBox1.LoadAsync("img/image.png");
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string result = "";
+            Task.Factory.StartNew(() => {
+                picloading.BeginInvoke(new Action(() =>
+                {
+                    picloading.Visible = true;
+                }));
+
+                result = OCR(Pix.LoadFromFile("img/image.png"));
+                richTextBox1.BeginInvoke(new Action(() => {
+
+                    richTextBox1.Text = result;
+
+                }));
+                picloading.BeginInvoke(new Action(() =>
+                {
+                    picloading.Visible = false;
+                }));
+
+            });
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                var settings = new MagickReadSettings();
+                // Settings the density to 300 dpi will create an image with a better quality
+                settings.Density = new Density(300);
+
+                using (var images = new MagickImageCollection())
+                {
+                    // Add all the pages of the pdf file to the collection
+                    images.Read(openFileDialog1.FileName, settings);
+
+                    /*    // Create new image that appends all the pages horizontally
+                        using (var horizontal = images.AppendHorizontally())
+                        {
+                            // Save result as a png
+                            horizontal.Write("img/image.png");
+                        }*/
+
+                    // Create new image that appends all the pages vertically
+                    using (var vertical = images.AppendVertically())
+                    {
+                        // Save result as a png
+                        vertical.Write("img/image.png");
+                    }
+                }
+            }
+           
+        }
+    }
+}
